9fa4de44233ab0b68daea8a12fce4e399d0dd176
Olme Marin <olme_gms@hotmail.com>
2019-10-09 20:44:07 -0500

    crud post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -57,9 +57,7 @@ class PostController extends Controller
 
         if (!empty($params_array)) {
             //Conseguir el usuario identificado
-            $jwtAuth  = new JwtAuth();
-            $token    = $request->header('Authorization', null);
-            $user     = $jwtAuth->checkToken($token, true);
+            $user = $this->getIdentity($request);
 
             //Validar los datos
             $validate = \Validator::make($params_array, [
@@ -94,9 +92,6 @@ class PostController extends Controller
 
             }
             
-
-            
-
         } else {
             $data = [
                 'code'       => 400,
@@ -107,5 +102,114 @@ class PostController extends Controller
 
         return response()->json($data, $data['code']);
 
+    }
+
+    public function update($id, Request $request){
+
+        //Recoger los datos de post
+        $json = $request->getContent();
+        $params_array = json_decode($json, true);
+
+        if (!empty($params_array)) {
+            // Validar los datos
+            $validate = \Validator::make($params_array, [
+                'title'         => 'required',
+                'content'       => 'required',
+                'category_id'   => 'required'
+            ]);
+
+            if ($validate->fails()) {
+                $data = [
+                    'code'       => 400,
+                    'status'     => 'error',
+                    'message'    => 'No has enviado el post'
+                ];
+            } else {
+                
+                //Eliminar lo que no queremos acrualizar 
+                unset($params_array['id']);
+                unset($params_array['user_id']);
+                unset($params_array['created_at']);
+                unset($params_array['user']);
+
+                //Conseguir el usuario identificado
+                $user = $this->getIdentity($request);
+
+                //Conseguri el post
+                $post = Post::where('id', $id)->where('user_id', $user->sub)->first();
+
+                if (!empty($post) && is_object($post)) {
+                    //Acutalizar post
+                    $post = Post::where('id', $id)->updateOrCreate($params_array);
+
+                    $data = [
+                        'code'       => 200,
+                        'status'     => 'success',
+                        'post'       => $post
+                    ];
+                } else {
+                    $data = [
+                        'code'       => 400,
+                        'status'     => 'error',
+                        'message'    => 'No tienes eres el creador del post'
+                    ];
+                }
+                
+
+                
+            }
+            
+
+        } else {
+            $data = [
+                'code'       => 400,
+                'status'     => 'error',
+                'message'    => 'No has enviado el post'
+            ];
+        }
+        
+
+        
+
+        return response()->json($data, $data['code']);
+    }
+
+
+    public function destroy($id, Request $request){
+
+        //Conseguir usuario identificado
+        $user = $this->getIdentity($request);
+
+        //Conseguri el post
+        $post = Post::where('id', $id)->where('user_id', $user->sub)->first();
+        if (!empty($post)) {
+           //Borrar post
+            $post->delete();
+            $data = [
+                'code'       => 200,
+                'status'     => 'success',
+                'post'       => $post
+            ];
+        } else {
+            $data = [
+                'code'       => 400,
+                'status'     => 'error',
+                'message'    => 'No se encontro el post que deseas eliminar'
+            ];
+        }
+        
+
+        
+
+        return response()->json($data, $data['code']);
+    } 
+    
+    
+    private function getIdentity(Request $request){
+        $jwtAuth  = new JwtAuth();
+        $token    = $request->header('Authorization', null);
+        $user     = $jwtAuth->checkToken($token, true);
+
+        return $user;
     }
 }
--- app/Post.php
+++ app/Post.php
@@ -8,6 +8,10 @@ class Post extends Model
 {
     protected $table = 'posts';
 
+    protected $fillable = [
+        'category_id','title', 'content', 'image','updated_at',
+    ];
+
     // Relación de uno a muchos inversa de muchos a uno
     public function user(){
         return $this->belongsTo('App\User', 'user_id');
